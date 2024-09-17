4897cd342346a41f2dc158589b6bdc8381d78602
18Mica <lopezaranamicaelabelen@gmail.com>
2024-09-17 12:24:59 -0300

    feat: agregar codigo 02_systick_blinky

--- workspace_lpc845/02_systick_blinky/source/02_systick_blinky.c
+++ workspace_lpc845/02_systick_blinky/source/02_systick_blinky.c
@@ -3,29 +3,48 @@
 #include "pin_mux.h"
 #include "fsl_debug_console.h"
 
-// Etiqueta para el LED azul
+
+// Pin para el LED azul
 #define LED_BLUE	1
-
-/*
- * @brief   Application entry point.
-*/
+#define LED_D1	29
+/**
+ * @brief Programa principal
+ */
 int main(void) {
+
 	// Inicializacion
-    BOARD_InitBootClocks();
-    BOARD_InitDebugConsole();
+	BOARD_BootClockFRO24M();
 
-    // Estructura de configuracion para salida
-    gpio_pin_config_t config = { kGPIO_DigitalOutput, 1 };
-    // Habilito el clock del GPIO 1
+    // Estructura de configuracion de entrada
+    gpio_pin_config_t out_config = { kGPIO_DigitalOutput, 1 };
+
+    // Habilito el puerto 1
     GPIO_PortInit(GPIO, 1);
-    // Configuro el pin 0 del GPIO 1 como salida
-    GPIO_PinInit(GPIO, 1, LED_BLUE, &config);
+    // Configuro LED como salida
+    GPIO_PinInit(GPIO, 0, 29, &out_config);
 
-    while(1) {
-    	// Cambio el estado anterior del LED azul
-    	GPIO_PinWrite(GPIO, 1, LED_BLUE, !GPIO_PinRead(GPIO, 1, LED_BLUE));
-    	// Demora
-    	for(uint32_t i = 0; i < 5000; i++);
-    }
-    return 0;
+    // Configuro SysTick para 1 ms
+    SysTick_Config(SystemCoreClock * 0.5);
+
+    while(1);
+    return 0 ;
+}
+
+/**
+ * @brief Handler para interrupcion de SysTick
+ */
+void SysTick_Handler(void) {
+	// Variable para contar interrupciones
+	static uint16_t i = 0;
+
+	// Incremento contador
+	i++;
+	// Verifico si el SysTick se disparo 500 veces (medio segundo)
+	!GPIO_PinRead(GPIO, 0, 29);
+	if(i == 3) {
+		// Reinicio el contador
+		i = 0;
+		// Conmuto los LEDS
+		GPIO_PinWrite(GPIO, 0, 29);
+	}
 }
